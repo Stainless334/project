77e70cdb6554886e90d58b7774757f54cd80c5ad
Stainless334 <lordstainless11@gmail.com>
2026-09-01 17:07:10 +0100

    expense

---
+++ expense.py
@@ -0,0 +1,31 @@
+Expense_1 = {
+    "Name": "School fees",
+    "Amount": 35000,
+    "Category": "Children"
+}
+
+print(Expense_1)
+'''
+print("Enter name of expense.")
+name = input()
+
+print("Enter amount of the expense.")
+amount = input()
+
+print("Enter the expense category.")
+cat = input()
+
+Expense_2 = {
+    "Name": name,
+    "Amount": float(amount),
+    "Category": cat
+}
+'''
+Expense_2 = {
+    "Name": "Lunch",
+    "Amount": 2500,
+    "Category": "Food"
+}
+print(Expense_2)
+
+the_list = [Expense_1, Expense_2]
